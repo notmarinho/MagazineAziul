aa0fd1f81c7c2a584d4c511a301639c45c77b8bd
Mateus <40375289+notmarinho@users.noreply.github.com>
2023-02-17 23:57:13 -0300

    Absolute Path & Folder Structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
