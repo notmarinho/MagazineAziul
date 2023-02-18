f4a670ffeb7da3d40727475783ef77fd65c8ed09
Mateus <40375289+notmarinho@users.noreply.github.com>
2023-02-17 23:44:17 -0300

    ESLint Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['simple-import-sort'],
+  rules: {
+    //Code style
+    'arrow-body-style': ['error', 'as-needed'],
+
+    //Typescript
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+
+    //Simple import sort
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          [
+            '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+          ],
+          ['^react', '^react-native'],
+          ['^@(react|react-native)-(.*)'],
+          ['^@.*'],
+          ['^~/.*'],
+          ['^\\u0000'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+  },
 };
