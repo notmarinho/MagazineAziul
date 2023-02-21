c456f6aa16de7b3d489a7c27fd50bc99562f452e
Mateus <40375289+notmarinho@users.noreply.github.com>
2023-02-21 17:08:58 -0300

    SafeArea Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {SafeAreaView} from 'react-native';
 
 import {store} from '@store/redux';
 
@@ -9,13 +8,11 @@ import Navigator from './navigation';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Provider store={store}>
-        <AppContextProvider>
-          <Navigator />
-        </AppContextProvider>
-      </Provider>
-    </SafeAreaView>
+    <Provider store={store}>
+      <AppContextProvider>
+        <Navigator />
+      </AppContextProvider>
+    </Provider>
   );
 }
 
